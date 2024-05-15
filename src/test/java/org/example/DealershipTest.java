436c18d484cc24080741ba35b525f39d4d895b4c
CISC190 Lecture <sophiaskedd05@gmail.com>
2024-05-15 10:56:26 -0700

    dealership test

---
+++ src/test/java/org/example/DealershipTest.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DealershipTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addVehicle() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void saveInventoryToFile() {
+    }
+}
